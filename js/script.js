0fce1c7cfa091994d811dec28e62f53eb432a33d
keisuke19860129 <takeharakeisuke@gmail.com>
2021-06-06 22:30:11 +0900

    add

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    $(function() {
+    $('.l-sidebar__hamburger').on('click',function(){
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.c-menu__list__slider,.c-slider__menu,.c-slider__sub-menu-item,.l-sidebar').addClass('active');
+        } else {
+            $('.c-menu__list__slider,.c-slider__menu,.c-slider__sub-menu-item,.l-sidebar').removeClass('active');
+        }
+    });
+});
+};
